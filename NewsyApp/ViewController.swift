d6db99cb41fa4f29de5e7d23149814faa9cc402c
deshawashington <washind14@icloud.com>
2022-05-16 17:53:54 -0600

    Initial Commit

---
+++ NewsyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsyApp
+//
+//  Created by Desha Washington on 5/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
